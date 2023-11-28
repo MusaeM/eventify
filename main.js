784dfaeb8279ab7c74efcadab73a768b505bd74a
MusaeM <70535571+MusaeM@users.noreply.github.com>
2023-11-28 21:44:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let currentFolder = null;
+let foldersData = []; // Array to store folder information
 
 // Function to display folders in tabular form
-function displayFolders() {
-    const folderListContainer = document.getElementById('folderList');
+async function displayFolders() {
+    const folderListContainer = document.getElementById('taskList');
+
+    // Fetch folders from the 'data' directory
+    const response = await fetch('data/');
+    const folderList = await response.json();
 
     // Create a table for the folder list
     const table = document.createElement('table');
-    table.id = 'folderListTable';
+    table.id = 'taskListTable';
 
     // Create table headers
     const thead = document.createElement('thead');
     const headerRow = document.createElement('tr');
-    const headers = ['Folder Name', 'Actions'];
+    const headers = ['Name', 'Actions'];
 
     headers.forEach(headerText => {
         const th = document.createElement('th');
@@ -25,12 +30,13 @@ function displayFolders() {
     // Create table body
     const tbody = document.createElement('tbody');
 
-    foldersData.forEach(folder => {
+    folderList.forEach(folderName => {
         const tr = document.createElement('tr');
-        tr.innerHTML = `<td>${folder.name}</td>
+        tr.innerHTML = `<td>${folderName}</td>
                         <td>
-                            <button onclick="editFolder('${folder.name}')">Edit</button>
-                            <button onclick="deleteFolder('${folder.name}')">Delete</button>
+                            <button onclick="openFolder('${folderName}')">Open</button>
+                            <button onclick="editFolder('${folderName}')">Edit</button>
+                            <button onclick="deleteFolder('${folderName}')">Delete</button>
                         </td>`;
         tbody.appendChild(tr);
     });
@@ -42,43 +48,12 @@ function displayFolders() {
     folderListContainer.appendChild(table);
 }
 
-// Function to create a new folder
-function createFolder() {
-    const newFolderName = document.getElementById('newFolderName').value.trim();
-
-    if (newFolderName !== '') {
-        // Check if the folder already exists
-        const folderExists = foldersData.some(folder => folder.name === newFolderName);
-
-        if (!folderExists) {
-            // Create a new folder and its corresponding JSON file
-            foldersData.push({ name: newFolderName });
-            createFolderFile(newFolderName);
-            displayFolders();
-            openFolder(newFolderName); // Open the newly created folder
-        } else {
-            alert('Folder already exists.');
-        }
-    }
-
-    // Clear the input field
-    document.getElementById('newFolderName').value = '';
-}
-
-// Function to create a JSON file for a new folder
-async function createFolderFile(folderName) {
-    try {
-        const jsonData = JSON.stringify([]);
-        await fetch(`data/${folderName}_entries.json`, {
-            method: 'PUT',
-            body: jsonData,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    } catch (error) {
-        console.error(`Error creating JSON file for folder ${folderName}:`, error);
-    }
+// Function to open a folder
+function openFolder(folderName) {
+    currentFolder = folderName;
+    document.getElementById('taskListContainer').style.display = 'none';
+    document.getElementById('folderContent').style.display = 'block';
+    document.getElementById('entryForm').style.display = 'block';
 }
 
 // Function to edit a folder
@@ -86,35 +61,24 @@ function editFolder(folderName) {
     const updatedFolderName = prompt('Enter the new folder name:', folderName);
 
     if (updatedFolderName !== null && updatedFolderName !== '') {
-        // Update the folder name and its corresponding JSON file
-        const folderIndex = foldersData.findIndex(folder => folder.name === folderName);
-        if (folderIndex !== -1) {
-            const oldFolderName = foldersData[folderIndex].name;
-            foldersData[folderIndex].name = updatedFolderName;
-            updateFolderFile(oldFolderName, updatedFolderName);
-            displayFolders();
-        }
-    }
-}
-
-// Function to update the JSON file for a folder
-async function updateFolderFile(oldFolderName, newFolderName) {
-    try {
-        // Fetch existing entries from the old folder
-        const entries = await fetchEntries(oldFolderName);
-
-        // Create the new folder and its corresponding JSON file
-        await createFolderFile(newFolderName);
-
-        // Save the existing entries to the new folder
-        if (entries) {
-            await saveEntries(newFolderName, entries);
-        }
-
-        // Delete the JSON file for the old folder
-        await deleteFolderFile(oldFolderName);
-    } catch (error) {
-        console.error(`Error updating JSON files for folders:`, error);
+        // Rename the folder
+        const oldFolderPath = `data/${folderName}_entries.json`;
+        const newFolderPath = `data/${updatedFolderName}_entries.json`;
+        fetch(oldFolderPath)
+            .then(response => response.json())
+            .then(entries => {
+                fetch(newFolderPath, {
+                    method: 'PUT',
+                    body: JSON.stringify(entries),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                    .then(() => deleteFolder(folderName))
+                    .then(() => displayFolders())
+                    .catch(error => console.error(`Error updating folder:`, error));
+            })
+            .catch(error => console.error(`Error fetching entries for folder:`, error));
     }
 }
 
@@ -123,162 +87,129 @@ function deleteFolder(folderName) {
     const confirmDelete = confirm(`Are you sure you want to delete the folder '${folderName}'?`);
 
     if (confirmDelete) {
-        // Delete the folder and its corresponding JSON file
-        foldersData = foldersData.filter(folder => folder.name !== folderName);
-        deleteFolderFile(folderName);
-        displayFolders();
-
-        // If the deleted folder is the current folder, reset the app
-        if (currentFolder === folderName) {
-            resetApp();
-        }
-    }
-}
-
-// Function to delete the JSON file for a folder
-async function deleteFolderFile(folderName) {
-    try {
-        await fetch(`data/${folderName}_entries.json`, {
+        const folderPath = `data/${folderName}_entries.json`;
+        fetch(folderPath, {
             method: 'DELETE',
-        });
-    } catch (error) {
-        console.error(`Error deleting JSON file for folder ${folderName}:`, error);
+        })
+            .then(() => displayFolders())
+            .catch(error => console.error(`Error deleting folder:`, error));
     }
 }
 
 // Function to reset the app
 function resetApp() {
     currentFolder = null;
-    document.getElementById('passwordForm').style.display = 'none';
-    document.getElementById('folderContent').style.display = 'none';
-    document.getElementById('folderList').style.display = 'block';
-}
-
-// Function to cancel folder creation
-function cancelFolderCreation() {
-    document.getElementById('folderForm').style.display = 'none';
-    document.getElementById('folderList').style.display = 'block';
-}
-
-// Function to open a folder
-function openFolder(folderName) {
-    currentFolder = folderName;
-    document.getElementById('folderList').style.display = 'none';
-    document.getElementById('folderForm').style.display = 'none';
-    document.getElementById('passwordForm').style.display = 'block';
-
-    // Clear the password input field
     document.getElementById('folderPassword').value = '';
+    document.getElementById('taskListContainer').style.display = 'block';
+    document.getElementById('folderContent').style.display = 'none';
+    document.getElementById('entryForm').style.display = 'none';
 }
+
+// Initial display of folders
+displayFolders();
+
+// Existing functions for managing tasks (addTask, editTask, deleteTask, etc.) go here
 
 // Function to check the password and proceed to the folder content
 function checkPassword() {
-    const password = document.getElementById('folderPassword').value;
+    const passwordInput = document.getElementById('folderPassword').value;
 
     // Placeholder logic: Assume password checking logic here (replace with your own logic)
     const isPasswordCorrect = true; // Replace with actual password validation
 
     if (isPasswordCorrect) {
-        document.getElementById('passwordForm').style.display = 'none';
+        document.getElementById('taskListContainer').style.display = 'none';
         document.getElementById('folderContent').style.display = 'block';
-        displayEntries();
+        document.getElementById('entryForm').style.display = 'block';
     } else {
         alert('Incorrect password. Please try again.');
     }
 }
 
-// Function to display entries for the current folder
-async function displayEntries() {
-    const folderContentContainer = document.getElementById('folderContent');
-
-    // Fetch entries for the current folder
-    const entries = await fetchEntries(currentFolder);
-
-    // Create a table for the entry list
-    const table = document.createElement('table');
-    table.id = 'folderContentTable';
-
-    // Create table headers
-    const thead = document.createElement('thead');
-    const headerRow = document.createElement('tr');
-    const headers = ['Name', 'Description', 'Points', 'User', 'Actions'];
-
-    headers.forEach(headerText => {
-        const th = document.createElement('th');
-        th.textContent = headerText;
-        headerRow.appendChild(th);
-    });
-
-    thead.appendChild(headerRow);
-    table.appendChild(thead);
-
-    // Create table body
-    const tbody = document.createElement('tbody');
-
-    entries.forEach(entry => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `<td>${entry.name}</td>
-                        <td>${entry.description}</td>
-                        <td>${entry.points}</td>
-                        <td>${entry.user}</td>
-                        <td>
-                            <button onclick="editEntry('${entry.name}')">Edit</button>
-                            <button onclick="deleteEntry('${entry.name}')">Delete</button>
-                        </td>`;
-        tbody.appendChild(tr);
-    });
-
-    table.appendChild(tbody);
-
-    // Append the table to the folder content container
-    folderContentContainer.innerHTML = '';
-    folderContentContainer.appendChild(table);
+// Function to show the add entry form
+function showAddForm() {
+    document.getElementById('entryForm').style.display = 'block';
 }
 
-// Function to fetch entries from a JSON file for a specific folder
-async function fetchEntries(folderName) {
-    try {
-        const response = await fetch(`data/${folderName}_entries.json`);
+// Function to add a task
+function addTask() {
+    const taskTable = document.getElementById('taskListTable');
+    const entryName = document.getElementById('entryName').value;
+    const entryDescription = document.getElementById('entryDescription').value;
+    const entryPoints = document.getElementById('entryPoints').value;
+    const entryUser = document.getElementById('entryUser').value;
 
-        if (!response.ok) {
-            throw new Error(`Error fetching entries for ${folderName}: ${response.statusText}`);
-        }
+    // Create a new row for the table
+    const newRow = taskTable.insertRow();
 
-        const entries = await response.json();
-        return entries || [];
-    } catch (error) {
-        console.error(`Error fetching entries for ${folderName}:`, error);
-        return [];
-    }
+    // Assign entry name as the ID for the row
+    newRow.id = entryName;
+
+    // Insert cells in the row with corresponding data
+    newRow.insertCell(0).textContent = entryName;
+    newRow.insertCell(1).textContent = entryDescription;
+    newRow.insertCell(2).textContent = entryPoints;
+    newRow.insertCell(3).textContent = entryUser;
+
+    // Create a button for the "Action" column (Edit)
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.onclick = function () {
+        // Call the editTask function with the corresponding entry ID
+        editTask(entryName);
+    };
+
+    // Create a button for the "Action" column (Delete)
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function () {
+        // Call the deleteTask function with the corresponding entry ID
+        deleteTask(entryName);
+    };
+
+    // Create a div for the buttons and append them
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.classList.add('entry-buttons');
+    buttonsDiv.appendChild(editButton);
+    buttonsDiv.appendChild(deleteButton);
+
+    // Insert the buttons div in the "Action" column
+    newRow.insertCell(4).appendChild(buttonsDiv);
+
+    // Clear the form and hide it
+    document.getElementById('taskForm').reset();
+    document.getElementById('entryForm').style.display = 'none';
 }
 
-// Function to create or update entries in a JSON file for a specific folder
-async function saveEntries(folderName, entries) {
-    try {
-        const jsonData = JSON.stringify(entries);
-        await fetch(`data/${folderName}_entries.json`, {
-            method: 'PUT',
-            body: jsonData,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    } catch (error) {
-        console.error(`Error saving entries for ${folderName}:`, error);
-    }
+// Function to edit a task
+function editTask(entryName) {
+    // Retrieve the row corresponding to the entryName
+    const taskRow = document.getElementById(entryName);
+
+    // Extract existing data from the row
+    const existingName = taskRow.cells[0].textContent;
+    const existingDescription = taskRow.cells[1].textContent;
+    const existingPoints = taskRow.cells[2].textContent;
+    const existingUser = taskRow.cells[3].textContent;
+
+    // Populate the form with existing data
+    document.getElementById('entryName').value = existingName;
+    document.getElementById('entryDescription').value = existingDescription;
+    document.getElementById('entryPoints').value = existingPoints;
+    document.getElementById('entryUser').value = existingUser;
+
+    // Show the form for editing
+    document.getElementById('entryForm').style.display = 'block';
+
+    // Remove the existing row from the table
+    taskRow.parentElement.removeChild(taskRow);
 }
 
-// Function to edit an entry
-function editEntry(entryName) {
-    // Assume entry editing logic here (replace with your own logic)
-    alert(`Editing entry '${entryName}'.`);
-}
+// Function to delete a task
+function deleteTask(entryName) {
+    // Retrieve the row corresponding to the entryName
+    const taskRow = document.getElementById(entryName);
 
-// Function to delete an entry
-function deleteEntry(entryName) {
-    // Assume entry deletion logic here (replace with your own logic)
-    alert(`Deleting entry '${entryName}'.`);
+    // Remove the row from the table
+    taskRow.parentElement.removeChild(taskRow);
 }
-
-// Initial display of folders
-displayFolders();
